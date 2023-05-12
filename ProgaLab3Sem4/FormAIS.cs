a5cb20a889d22ebd9dcd885c64c795abd1a85b60
user <batunin.diman2003@gmail.com>
2023-05-13 02:31:37 +0700

    вава

--- ProgaLab3Sem4/FormAIS.cs
+++ ProgaLab3Sem4/FormAIS.cs
@@ -4,18 +4,30 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using DLL;
 
 namespace ProgaLab2Sem4
 {
     public partial class FormAIS : Form
     {
+        public delegate void AISDelegate(string filePath, ToolStrip strip);
         public FormAIS(string filePath)
         {
             InitializeComponent();
+            //Assembly asm = Assembly.LoadFrom("C:\\Проги\\DLLProgaLab3Sem4\\DLLProgaLab3Sem4\\bin\\Debug\\DLLProgaLab3Sem4.dll");
+            //Type? t = asm.GetType("DLL.Menu");
+            //if (t is not null)
+            //{
+            //    // получаем метод Square
+            //    MethodInfo? menu = t.GetMethod("StaticCreateMenu");
+
+            //    AISDelegate aisGelegate = menu.CreateDelegate<AISDelegate>();
+            //    aisGelegate(filePath, menuStrip1);
+            //}
+
             DLL.Menu menu = new DLL.Menu(filePath, menuStrip1);
             menu.CreateMenu();
         }
--- ProgaLab3Sem4/FormAutorization.cs
+++ ProgaLab3Sem4/FormAutorization.cs
@@ -10,12 +10,13 @@ using System.Windows.Forms;
 using System.Configuration;
 using System.Reflection.Emit;
 using System.Reflection;
-//using DLL;
+using DLL;
 
 
 namespace ProgaLab2Sem4
 {
     public delegate void Initialization(bool result, string menuPath);
+    public delegate bool AutorizationDelegate(string userName, string Password, out string menuPath, string filePath );
     public partial class FormAutorization : Form
     {
         string fileUsersPath;
@@ -29,28 +30,22 @@ namespace ProgaLab2Sem4
         private void btnEntry_Click(object sender, EventArgs e)
         {
             //Assembly asm = Assembly.LoadFrom("C:\\Проги\\DLLProgaLab3Sem4\\DLLProgaLab3Sem4\\bin\\Debug\\DLLProgaLab3Sem4.dll");
-            //string menuPath = "";
-            //Type t = asm.GetType("FileAutorization.Autorization");
-            //ConstructorInfo dynamicConstructor = t.GetConstructor(new Type[] { typeof(string) });
-            //var result = dynamicConstructor.Invoke(new object[] { txtBoxUserName.Text, txtBoxPassword.Text, menuPath });
-            //wasInitialization.Invoke((bool)result, menuPath);
+            //Type? t = asm.GetType("DLL.FileAutorization");
+            //if (t is not null)
+            //{
+            //    // получаем метод Square
+            //    MethodInfo? Autorization = t.GetMethod("StaticAutorization");
 
-            Assembly asm = Assembly.LoadFrom("C:\\Проги\\DLLProgaLab3Sem4\\DLLProgaLab3Sem4\\bin\\Debug\\DLLProgaLab3Sem4.dll");
-            Type? t = asm.GetType("FileAutorization");
-            if (t is not null)
-            {
-                // получаем метод Square
-                MethodInfo? Autorization = t.GetMethod("Autorization", BindingFlags.Public);
+            //    string menuPath = "";
+            //    AutorizationDelegate autorizationGelegate = Autorization.CreateDelegate<AutorizationDelegate>();
+            //    bool result = autorizationGelegate(txtBoxUserName.Text, txtBoxPassword.Text, out menuPath, "C:\\Users\\user\\Desktop\\Users.txt");
+            //    wasInitialization.Invoke(result, menuPath);
+            //}
 
-                string menuPath = "";
-                object? result = Autorization?.Invoke(null, new object[] { txtBoxUserName.Text, txtBoxPassword.Text, menuPath });
-                wasInitialization.Invoke((bool)result, menuPath);
-            }
-
-            //FileAutorization fileAutharization = new FileAutorization(fileUsersPath);
-            //string menuPath;
-            //bool result = fileAutharization.Autorization(txtBoxUserName.Text, txtBoxPassword.Text, out menuPath);
-            //wasInitialization.Invoke(result, menuPath);
+            FileAutorization fileAutharization = new FileAutorization(fileUsersPath);
+            string menuPath;
+            bool result = fileAutharization.Autorization(txtBoxUserName.Text, txtBoxPassword.Text, out menuPath);
+            wasInitialization.Invoke(result, menuPath);
         }
         private void btnCancel_Click(object sender, EventArgs e)
         {
